7e2aa238f383431f9fa19735403c5d54cd334361
Borlehandro <borlehandro@gmail.com>
2023-02-06 20:16:15 +0700

    add fonts and image

--- iosApp/iosApp/PresentationLayer/View/ContentView.swift
+++ iosApp/iosApp/PresentationLayer/View/ContentView.swift
@@ -5,16 +5,29 @@ struct ContentView: View {
     let greet = "Test"
     @StateObject var viewModel = MainViewModel()
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("City name")
-                Text("День недели")
-                Text("Температура \(viewModel.temperature)")
+        ScrollView() {
+            VStack() {
+                VStack(alignment: .leading) {
+                    Text("City name")
+                        .font(.custom("Inter-Bold", size: 20))
+                    Text("\(Date().formatted(.dateTime.day().month()))")
+                }
+                HStack(alignment: .center) {
+                    Image("rainyImage")
+                        .resizable()
+                        .scaledToFit()
+                    VStack {
+                        Text(viewModel.temperature)
+                            .font(.custom("Inter-Bold", size: 43))
+                            .padding(0)
+                        Text(viewModel.type)
+                    }
+                }
                 Text(viewModel.windSpeed)
                 Text(viewModel.rainFall)
-                Text(viewModel.type)
-            }
+            }.frame(maxWidth: .infinity)
         }
+        .background(Color.mint.ignoresSafeArea())
     }
 }
 
