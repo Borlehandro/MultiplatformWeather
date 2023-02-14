1f816b66032a77c2fc859373ef6b633d9f793299
Borlehandro <borlehandro@gmail.com>
2023-02-14 16:45:58 +0700

    Core hotfix: Fix iOS HttpEngineFactory

--- core/src/iosMain/kotlin/io/decentury/mutliplatform/weatherkmm/network/HttpEngineFactory.kt
+++ core/src/iosMain/kotlin/io/decentury/mutliplatform/weatherkmm/network/HttpEngineFactory.kt
@@ -4,7 +4,7 @@ import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.darwin.Darwin
 
-class HttpEngineFactory actual constructor() {
+actual class HttpEngineFactory actual constructor() {
 
     actual fun createEngine(): HttpClientEngineFactory<HttpClientEngineConfig> = Darwin
 }
